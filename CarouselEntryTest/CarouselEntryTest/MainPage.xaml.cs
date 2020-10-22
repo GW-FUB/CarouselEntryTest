3cd20591a1493dc6a827fc6e576c7d0ab7db97b4
GW <gw@fub.de>
2020-10-22 15:54:29 +0200

    Projektdateien hinzufügen.

---
+++ CarouselEntryTest/CarouselEntryTest/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace CarouselEntryTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Carousel.ItemsSource = new List<string> { "0", "1", "2" };
+        }
+    }
+}
